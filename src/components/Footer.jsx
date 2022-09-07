35f4191289dc2cd5f0ac64aa19defe1e9ba10510
myselfshravan <shravanrevanna@gmail.com>
2022-09-07 11:19:07 +0530

    check 1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ export function Footer() {
     <footer className="py-16">
       <Container className="flex flex-col items-center justify-between md:flex-row">
         {/* <Logo className="h-12 w-auto text-slate-900" /> */}
-        
+
         <p className="mt-6 text-base text-slate-500 md:mt-0">
-          WebPage Designed by Shravan
+          WebPage Designed and Developed by Shravan
         </p>
       </Container>
     </footer>
