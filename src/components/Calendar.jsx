39d1071deb2a9783d1a9b7a466bf685980a1fd58
myselfshravan <shravanrevanna@gmail.com>
2023-04-12 21:40:00 +0530

    calender changed and added dates

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -13,7 +13,7 @@ let todaydate = newDate.toISOString().slice(0, 10)
 let currentmonth = newDate.getMonth() + 1
 
 const datesarray = []
-for (let i = 27; i <= 100; i++) {
+for (let i = 34; i <= 100; i++) {
   datesarray.push(
     new Date(newDate.getFullYear(), 2, i).toISOString().slice(0, 10)
   )
@@ -31,8 +31,32 @@ const weekarray = [
 
 const exam = [
   {
-    date: '',
-    name: '',
+    date: '2023-04-19',
+    name: 'Maths',
+  },
+  {
+    date: '2023-04-21',
+    name: 'UHV',
+  },
+  {
+    date: '2023-04-24',
+    name: 'DBMS',
+  },
+  {
+    date: '2023-04-26',
+    name: 'DS',
+  },
+  {
+    date: '2023-04-28',
+    name: 'COA',
+  },
+  {
+    date: '2023-05-02',
+    name: 'DMS',
+  },
+  {
+    date: '2023-05-04',
+    name: 'Kan',
   },
 ]
 
@@ -57,19 +81,15 @@ export function Calendar() {
         SEE Exam TimeTable
       </p>
       <div className="flex items-center justify-center px-6 py-3 lg:flex-none">
-        <h1 className="rounded-md bg-white px-3 py-1 text-lg font-semibold text-gray-900 dark:bg-white/30 dark:text-white">
+        <h1 className="rounded-md bg-white px-3 py-1 text-lg font-semibold text-gray-900 dark:bg-[#0071f0] dark:text-white">
           <time>{newDate.toLocaleString('default', { month: 'long' })}</time>
         </h1>
       </div>
-      <div className="rounded-xl bg-white p-2 dark:bg-slate-800">
+      <div className="rounded-xl bg-white p-2 dark:bg-gray-900 lg:p-3 border dark:border-gray-300">
         <div className="lg:flex lg:flex-auto lg:flex-col">
-          <div className="grid grid-cols-7 text-center text-xs font-semibold leading-6 text-gray-700 dark:bg-cost5 dark:text-white lg:flex-none">
+          <div className="grid grid-cols-7 text-center text-xs font-semibold leading-6 text-gray-700 dark:text-white lg:flex-none lg:rounded-t-lg">
             {weekarray.map((day) => (
-              <div
-                key={day.long}
-                className="border py-2 dark:border-indigo-50"
-                aria-hidden="true"
-              >
+              <div key={day.long} className="py-2" aria-hidden="true">
                 {day.short}
                 <span key={day.long} className="sr-only sm:not-sr-only">
                   {day.long}
@@ -77,24 +97,28 @@ export function Calendar() {
               </div>
             ))}
           </div>
-          <div className="flex bg-indigo-50 text-xs leading-6 text-gray-700 dark:bg-cost5 dark:text-zinc-50 lg:flex-auto">
+          <div className="flex text-xs leading-6 text-gray-700 dark:text-zinc-50 lg:flex-auto">
             {/* Desktop view */}
-            <div className="hidden w-full lg:grid lg:grid-cols-7 lg:grid-rows-5">
-              {days.map((day) => (
+            <div className="isolate hidden w-full gap-px rounded-lg bg-gray-200 shadow ring-1 ring-gray-200 dark:bg-slate-600 dark:bg-white/40 dark:ring-gray-100 lg:grid lg:grid-cols-7 lg:grid-rows-5">
+              {days.map((day, dayIdx) => (
                 <div
                   key={day.date}
                   className={classNames(
                     parseInt(day.date.slice(5, 7)) == currentmonth
                       ? 'bg-white dark:bg-slate-900'
-                      : 'bg-white/50 dark:bg-slate-800',
-                    'relative border px-3 py-2 dark:border-indigo-50'
+                      : 'bg-white/90 dark:bg-slate-800',
+                    dayIdx === 0 && 'rounded-tl-lg',
+                    dayIdx === 6 && 'rounded-tr-lg',
+                    dayIdx === days.length - 7 && 'rounded-bl-lg',
+                    dayIdx === days.length - 1 && 'rounded-br-lg',
+                    'px-3 py-2'
                   )}
                 >
                   <time
                     dateTime={day.date}
                     className={classNames(
                       todaydate == day.date
-                        ? 'flex h-6 w-6 items-center justify-center rounded-full bg-blue-700 font-semibold text-white'
+                        ? 'flex h-6 w-6 items-center justify-center rounded-full bg-[#0071f0] font-semibold text-white'
                         : undefined,
                       'px-1 text-sm font-medium'
                     )}
@@ -106,7 +130,7 @@ export function Calendar() {
                       {day.events.map((event) => (
                         <li>
                           <a href={event.href} className="group flex">
-                            <div className="truncate rounded-md bg-indigo-50 px-2 align-middle font-medium text-gray-900 group-hover:text-blue-700 dark:bg-white/30 dark:text-white">
+                            <div className="truncate rounded-md bg-indigo-50 px-2 align-middle font-medium text-gray-900 group-hover:text-blue-700 dark:bg-[#0071f0] dark:text-white">
                               <p className="text-sh">{event.name}</p>
                             </div>
                           </a>
@@ -118,14 +142,14 @@ export function Calendar() {
               ))}
             </div>
             {/* Mobile view */}
-            <div className="isolate grid w-full grid-cols-7 grid-rows-5 rounded-lg lg:hidden">
-              {days.map((day) => (
+            <div className="ring-grey-200 isolate grid w-full grid-cols-7 grid-rows-5 gap-px rounded-lg bg-slate-100 ring-1 dark:ring-white lg:hidden">
+              {days.map((day, dayIdx) => (
                 <div
                   key={day.date}
                   className={classNames(
                     parseInt(day.date.slice(5, 7)) == currentmonth
                       ? 'bg-white dark:bg-slate-900'
-                      : 'bg-white/50 dark:bg-slate-800',
+                      : 'bg-white/90 dark:bg-slate-800',
                     (day.isSelected || todaydate == day.date) &&
                       'font-semibold',
                     day.isSelected && 'text-white',
@@ -140,7 +164,11 @@ export function Calendar() {
                       !parseInt(day.date.slice(5, 7)) == currentmonth &&
                       !todaydate == day.date &&
                       'text-gray-500 dark:text-gray-400',
-                    'flex h-16 flex-col items-center border px-3 py-2 align-middle hover:bg-gray-100 focus:z-10 dark:border-indigo-50'
+                    dayIdx === 0 && 'rounded-tl-lg',
+                    dayIdx === 6 && 'rounded-tr-lg',
+                    dayIdx === days.length - 7 && 'rounded-bl-lg',
+                    dayIdx === days.length - 1 && 'rounded-br-lg',
+                    'flex h-16 flex-col items-center px-3 py-2 align-middle hover:bg-gray-100 focus:z-10'
                   )}
                 >
                   <time
@@ -167,35 +195,6 @@ export function Calendar() {
           </div>
         </div>
       </div>
-
-      {selectedDay?.events.length > 0 && (
-        <div className="px-4 py-10 sm:px-6 lg:hidden">
-          <ol className="divide-y divide-gray-100 overflow-hidden rounded-lg bg-white text-sm shadow ring-1 ring-black ring-opacity-5">
-            {selectedDay.events.map((event) => (
-              <li
-                key={event.id}
-                className="group flex p-4 pr-6 focus-within:bg-gray-50 hover:bg-gray-50"
-              >
-                <div className="flex-auto">
-                  <p className="font-semibold text-gray-900">{event.name}</p>
-                  <time
-                    dateTime={event.datetime}
-                    className="mt-2 flex items-center text-gray-700"
-                  >
-                    {event.time}
-                  </time>
-                </div>
-                <a
-                  href={event.href}
-                  className="ml-6 flex-none self-center rounded-md border border-gray-300 bg-white px-3 py-2 font-semibold text-gray-700 opacity-0 shadow-sm hover:bg-gray-50 focus:opacity-100 group-hover:opacity-100"
-                >
-                  Edit<span className="sr-only">, {event.name}</span>
-                </a>
-              </li>
-            ))}
-          </ol>
-        </div>
-      )}
     </div>
   )
 }
